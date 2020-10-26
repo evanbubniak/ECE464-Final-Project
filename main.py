3c3e9f6d654c33e7f4d30de448b40f22009e1e4b
Evan Bubniak <evan.bubniak@gmail.com>
2020-10-26 18:41:42 -0400

    add API call to script

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-API_KEY = open("news_api_key.txt", "r").read()
+API_KEY = open("news_api_key.txt", "r").read()[:-1]
 print(API_KEY)
+
+
+from newsapi import NewsApiClient
+
+# Init
+newsapi = NewsApiClient(api_key=API_KEY)
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q='bitcoin',
+                                          sources='bbc-news,the-verge',
+                                          language='en')
+
+# /v2/everything
+all_articles = newsapi.get_everything(q='bitcoin',
+                                      sources='bbc-news,the-verge',
+                                      domains='bbc.co.uk,techcrunch.com',
+                                      from_param='2020-10-01',
+                                      to='2020-10-26',
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=2)
+
+# /v2/sources
+sources = newsapi.get_sources()
+
+print(sources)
