f08cd20e9707e2482a3907beb00ac61e52ec0d53
NickTriano <nicholas.triano27@gmail.com>
2020-11-29 21:47:21 -0500

    More requests and populated Users collection

--- api_mongo.py
+++ api_mongo.py
@@ -38,9 +38,34 @@ class Article(db.Document):
             "url": self.url
         }
 
-# example request: http POST http://127.0.0.1:5000/api/db_populate
-@app.route('/api/db_populate', methods=['POST'])
-def db_populate():
+class User(db.Document):
+    username = db.StringField()
+    preferences = db.DictField() #or EmbeddedDocumentField()
+    # alternative: have one field per each potential preference instead of a dict
+
+    def to_json(self):
+        # convert document to JSON
+        return {
+            "username": self.username,
+            "preferences": self.preferences # need to do any additional parsing here?
+        }
+
+class Analytic(db.Document):
+    userId = db.StringField()
+    articleId = db.StringField()
+    accessDate = db.DateTimeField()
+
+    def to_json(self):
+        # convert document to JSON
+        return {
+            "userId": self.userId,
+            "articleId": self.articleId,
+            "accessDate": self.accessDate
+        }
+
+# example request: http POST http://127.0.0.1:5000/api/articles_populate
+@app.route('/api/articles_populate', methods=['POST'])
+def articles_populate():
     # may need to store the source url if using excludeDomains parameter later
     all_sources = [source['id'] for source in newsapi.get_sources()['sources']]
 
@@ -60,6 +85,27 @@ def db_populate():
     # gives a 404 error if not called with an http POST request, but db is populated successfully regardless
     return make_response("", 201)
 
+# example request: http POST http://127.0.0.1:5000/api/users_populate
+@app.route('/api/users_populate', methods=['POST'])
+def users_populate():
+    # ***News API has no way of excluding certain topics, so we can't have a topic blacklist currently
+    # should we make the preference field names consistent with the News API query parameter names?
+    users = []
+    preferences = {
+        'favoriteTopics': 'soccer',
+        'favoriteSources': ['bbc-sport'],
+    }
+    users.append(User(username='Nick', preferences=preferences))
+    preferences = {
+        'favoriteSources': ['associated-press', 'time'],
+        'excludedSources': 'fox-news'
+    }
+    users.append(User(username='Evan', preferences=preferences))
+    User.objects.insert(users)
+
+    # gives a 404 error if not called with an http POST request, but db is populated successfully regardless
+    return make_response("", 201)
+
 # basic GET/POST request, will need to incorporate input from the front-end later
 @app.route('/api/articles', methods=['GET', 'POST'])
 def api_books():
@@ -74,5 +120,39 @@ def api_books():
         article.save()
         return make_response("", 201)
 
+# basic GET/POST request, will need to incorporate input from the front-end later
+@app.route('/api/users', methods=['GET', 'POST'])
+def api_books():
+    if request.method == "GET":
+        users = []
+        for user in User.objects:
+            users.append(user)
+        return make_response(jsonify(users), 200)
+    elif request.method == "POST":
+        content = request.json
+        user = User(username=content['username'], preferences=content['preferences'])
+        user.save()
+        return make_response("", 201)
+
+# basic GET/POST request, will need to incorporate input from the front-end later
+@app.route('/api/analytics', methods=['GET', 'POST'])
+def api_books():
+    if request.method == "GET":
+        analytics = []
+        for analytic in Analytic.objects:
+            analytics.append(analytic)
+        return make_response(jsonify(analytics), 200)
+    elif request.method == "POST":
+        content = request.json
+        analytic = Analytic(userId=content['userId'], articleId=content['articleId'], accessDate=content['accessDate'])
+        analytic.save()
+        return make_response("", 201)
+
 if __name__ == '__main__':
     app.run()
+
+
+"""
+all sources from News API (comma-separated):
+'abc-news,abc-news-au,aftenposten,al-jazeera-english,ansa,argaam,ars-technica,ary-news,associated-press,australian-financial-review,axios,bbc-news,bbc-sport,bild,blasting-news-br,bleacher-report,bloomberg,breitbart-news,business-insider,business-insider-uk,buzzfeed,cbc-news,cbs-news,cnn,cnn-es,crypto-coins-news,der-tagesspiegel,die-zeit,el-mundo,engadget,entertainment-weekly,espn,espn-cric-info,financial-post,focus,football-italia,fortune,four-four-two,fox-news,fox-sports,globo,google-news,google-news-ar,google-news-au,google-news-br,google-news-ca,google-news-fr,google-news-in,google-news-is,google-news-it,google-news-ru,google-news-sa,google-news-uk,goteborgs-posten,gruenderszene,hacker-news,handelsblatt,ign,il-sole-24-ore,independent,infobae,info-money,la-gaceta,la-nacion,la-repubblica,le-monde,lenta,lequipe,les-echos,liberation,marca,mashable,medical-news-today,msnbc,mtv-news,mtv-news-uk,national-geographic,national-review,nbc-news,news24,new-scientist,news-com-au,newsweek,new-york-magazine,next-big-future,nfl-news,nhl-news,nrk,politico,polygon,rbc,recode,reddit-r-all,reuters,rt,rte,rtl-nieuws,sabq,spiegel-online,svenska-dagbladet,t3n,talksport,techcrunch,techcrunch-cn,techradar,the-american-conservative,the-globe-and-mail,the-hill,the-hindu,the-huffington-post,the-irish-times,the-jerusalem-post,the-lad-bible,the-next-web,the-sport-bible,the-times-of-india,the-verge,the-wall-street-journal,the-washington-post,the-washington-times,time,usa-today,vice-news,wired,wired-de,wirtschafts-woche,xinhua-net,ynet'
+"""
